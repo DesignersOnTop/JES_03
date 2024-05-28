455c0b19d8a245ea41e63c8727d37df324b0f8d9
albiery <albieryrf@gmail.com>
2024-05-28 16:41:14 -0400

    'Habran mejoras'

--- js/ca-ho.js
+++ js/ca-ho.js
@@ -1,6 +1,7 @@
 // Selecciona todos los elementos con la clase 'btn-calificaciones' y 'btn-horario'
 let botonesCalificaciones = document.getElementsByClassName('btn-calificaciones');
 let botonesHorario = document.getElementsByClassName('btn-horario');
+let scroll_calificaciones = document.getElementsByClassName('botones-container');
 
 // Función para deseleccionar todos los botones
 function deseleccionarBotones() {
@@ -11,17 +12,19 @@ function deseleccionarBotones() {
 // Añade el evento 'click' a cada botón de calificaciones
 Array.from(botonesCalificaciones).forEach((boton) => {
     boton.addEventListener('click', () => {
-        document.querySelector('.ca-ho-scroll').scrollLeft -= 806;
+        document.querySelector('.ca-ho-scroll').scrollLeft -= 966;
         deseleccionarBotones(); // Deselecciona todos los botones
         boton.classList.add('btn-selected'); // Selecciona el botón actual
+        scroll_calificaciones[0].style.display = 'flex'; // display flex para que se muestre
     });
 });
 
 // Añade el evento 'click' a cada botón de horario
 Array.from(botonesHorario).forEach((boton) => {
     boton.addEventListener('click', () => {
-        document.querySelector('.ca-ho-scroll').scrollLeft += 806;
+        document.querySelector('.ca-ho-scroll').scrollLeft += 966;
         deseleccionarBotones(); // Deselecciona todos los botones
         boton.classList.add('btn-selected'); // Selecciona el botón actual
+        scroll_calificaciones[0].style.display = 'none'; //display none para ocultar
     });
 });
