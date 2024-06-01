b7621c59ac5ee712f0af699698b615045f6a1aca
DesignersOnTop <designers859@gmail.com>
2024-06-01 18:37:14 -0400

    'mas cerca de la terminacion'

--- js/scroll.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('scroll-left').addEventListener('click', () => {
-document.getElementById('scroll-container').scrollLeft -= 600;
-});
-
-document.getElementById('scroll-right').addEventListener('click', () => {
-document.getElementById('scroll-container').scrollLeft += 600;
-});
--- js/scroll_calificacion.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('scroll-izquierdo').addEventListener('click', () => {
-    document.querySelector('.tabla-calificacion-centrar').scrollLeft -= 160;
-});
-
-document.getElementById('scroll-derecho').addEventListener('click', () => {
-    document.querySelector('.tabla-calificacion-centrar').scrollLeft += 160;
-});
