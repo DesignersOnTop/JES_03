57ac97cdd658fa453dcffb7b807f71669db24935
DesignersOnTop <designers859@gmail.com>
2024-06-09 19:27:19 -0400

    'cambios'

--- templates/js/menu.js
+++ templates/js/menu.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('.menu');
 
 menu.addEventListener('click',() => {
     Swal.fire({
-        html: '<nav><ul class="ul-eme"><li class="calendario-container li-eme"><a href="#">Recordatorio</a></li><li class="li-eme"><a href="e-refuerzo-libros.html" class="a-eme home-eme">Refuerzo de curso</a></li><li class="li-eme"><a href="e-material_estudio.html" class="a-eme material-eme">Material de tareas</a></li><li class="li-eme"><a href="../index.html" class="a-eme cerrar-eme">Cerrar Sesion</a></li></ul></nav>',
+        html: '<nav><ul class="ul-eme"><li class="li-eme"><a href="../estudiante/e-perfil.html">Perfil</a></li><li class="calendario-container li-eme"><a href="e-recordatorio.html">Recordatorio</a></li><li class="li-eme"><a href="e-refuerzo-libros.html" class="a-eme home-eme">Refuerzo de curso</a></li><li class="li-eme"><a href="e-material_estudio.html" class="a-eme material-eme">Material de tareas</a></li><li class="li-eme"><a href="../index.html" class="a-eme cerrar-eme">Cerrar Sesion</a></li></ul></nav>',
         backdrop: true,
         customClass: {
             popup: 'emergente-class classes-eme',
