01aa02802b3042e01628dbeaa6c81dfdb6a873a8
Smailyn <smaburgos2003@gmail.com>
2024-06-28 15:17:31 -0400

    acomodar

--- templates/js/a-visualizar_imagenes.js
+++ templates/js/a-visualizar_imagenes.js
@@ -1,17 +1,17 @@
  document.getElementById('imagen-materia').addEventListener('change', function(event) {
-            const file = event.target.files[0];
-            const reader = new FileReader();
+    const file = event.target.files[0];
+    const reader = new FileReader();
 
-            reader.onload = function(e) {
-                const imgElement = document.createElement('img');
-                imgElement.src = e.target.result;
+    reader.onload = function(e) {
+    const imgElement = document.createElement('img');
+    imgElement.src = e.target.result;
 
-                const imageContainer = document.getElementById('imagen-materia-js');
-                imageContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar la nueva imagen
-                imageContainer.appendChild(imgElement);
-            };
+    const imageContainer = document.getElementById('imagen-materia-js');
+    imageContainer.innerHTML = ''; // Limpiar el contenedor antes de agregar la nueva imagen
+    imageContainer.appendChild(imgElement);
+};
 
-            if (file) {
-                reader.readAsDataURL(file);
-            }
-        });
+    if (file) {
+        reader.readAsDataURL(file);
+ }
+});
